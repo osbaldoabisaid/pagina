dde0b357cfd6c5dc1f4a24cb2e96a1a60d55c2b8
Osbaldo Said Pelcastre Cabrera <opelcastre@PRINTEXD.LOCAL>
2020-09-01 14:04:48 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    /*console.log("click");*/
+    document.querySelector(".nav-menu").classList.toggle("show");
+})
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".new-cards", {delay: 500});
+ScrollReveal().reveal(".cards-banner-one", {delay: 500});
+ScrollReveal().reveal(".cards-banner-two", {delay: 500});
+ScrollReveal().reveal(".footer-links", {delay: 500});
+ScrollReveal().reveal(".social", {delay: 500});
+ScrollReveal().reveal(".footer", {delay: 500});
